1ff4016d43fda8f413c2b0c794c11ae30009ce63
Jheyson Saavedra <jheysondev@users.noreply.github.com>
2021-08-09 20:48:43 -0500

    feat: add head vue plugin and chose to vite-ssr command on dev script

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import viteSSR from 'vite-ssr'
+import viteSSR, { ClientOnly } from 'vite-ssr'
+import { createHead, Head } from '@vueuse/head'
 import routes from '@/routes'
 import App from '@/App.vue'
 
-export default viteSSR(App, { routes })
+export default viteSSR(App, { routes }, ({ app }) => {
+  const head = createHead()
+  app.use(head)
+
+  app.component(ClientOnly.name, ClientOnly)
+  app.component(Head.name, Head)
+})
